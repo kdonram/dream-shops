6afe4350babbf77f36974061f6baabca244e4afc
kdrm <kdonram@gmail.com>
2025-02-21 13:49:01 +0530

    Create Image Attributes.

--- dream-shops/src/main/java/com/dailywork/dream_shops/Image.java
+++ dream-shops/src/main/java/com/dailywork/dream_shops/Image.java
@@ -1,4 +1,11 @@
 package com.dailywork.dream_shops;
 
+import java.sql.Blob;
+
 public class Image {
+    private Long id;
+    private String filename;
+    private String fileType;
+    private Blob image;
+    private String downloadUrl;
 }
